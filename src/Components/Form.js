b3d80f6041113de23bb29fa0839251b322419423
vikasthakurr <vikaskumar20012001@gmail.com>
2023-01-11 21:58:23 +0530

    changes

---
+++ src/Components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Form.css'
+
+const Form = () => {
+   return (
+      <div>
+         <h1>hello airlines wale</h1>
+      </div>
+   );
+};
+export default Form;
